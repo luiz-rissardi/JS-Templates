a64f6f8c841934c5b567e62adc65b4a3974120da
LuizRissardi <rissardi.luiz2006@gmail.com>
2023-03-10 15:08:19 -0300

    refatoracção de codigo para codigo limpo

--- JS-Templates/generates_functions/att.js
+++ JS-Templates/generates_functions/att.js
@@ -1,32 +1,28 @@
-function* generate() {
-    const response = yield fetch("https://jsonplaceholder.typicode.com/users")
-    const data = yield response.json()
+function* getRepoInGitHub(){
+    const resolve = yield fetch("https://api.github.com/users/timmywheels/repos")
+    const data = yield resolve.json()
     return data
 }
 
-function master(generator) {
-    return new Promise((resolve, reject) => {
+ProcessAsync(getRepoInGitHub).then(console.log)
+
+function ProcessAsync(generator){
+    return new Promise((resolve,reject)=>{
         try {
-            const generate = generator();
-            function generators(data) {
-                if (data.done) {
-                    resolve(data)
-                } else {
-                    resolve(data.value).then(response => {
-                        return generators(generate.next(response))
+            const initGenerator =  generator()
+            resolveNextGeneration(initGenerator.next())
+            function resolveNextGeneration(dataGenerate){
+                if(dataGenerate.done){
+                    resolve(dataGenerate.value)
+                }else{
+                    Promise.resolve(dataGenerate.value).then(response =>{
+                        resolveNextGeneration(initGenerator.next(response))
                     })
                 }
             }
-            generators(generate.next())
         } catch (error) {
-            reject(error)
+            reject( new Error(" um erro foi lançado ao ar"))
         }
     })
 }
 
-async function main() {
-    const data = await master(generate)
-    console.log(await data.json())
-}
-
-main()
